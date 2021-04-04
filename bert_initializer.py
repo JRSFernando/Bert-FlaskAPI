2b121ef7cd0ddace66b458c2b8a0d5cf8fab6432
JRSFernando <sunethf9@gmail.com>
2021-04-04 18:53:46 +0530

    definition of classification task

--- bert_initializer.py
+++ bert_initializer.py
@@ -3,3 +3,13 @@ from transformers import BertTokenizer, TFBertForSequenceClassification
 
 model = TFBertForSequenceClassification.from_pretrained('./fine-tuned-Bert/')
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+
+
+def classify_tweets(pred_sentences):
+    tf_batch = tokenizer(pred_sentences, max_length=75, padding=True, truncation=True, return_tensors='tf')
+    tf_outputs = model(tf_batch)
+    tf_predictions = tf.nn.softmax(tf_outputs[0], axis=-1)
+    label = tf.argmax(tf_predictions, axis=1)
+    label = label.numpy()
+
+    return label
